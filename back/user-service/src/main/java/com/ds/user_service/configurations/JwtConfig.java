1cfdbabe55fe9e806c1427d5f1757fe8b60c8751
Natidev <53361365+Natidev@users.noreply.github.com>
2026-01-02 01:36:56 +0300

    Refactor

--- back/user-service/src/main/java/com/ds/user_service/configurations/JwtConfig.java
+++ back/user-service/src/main/java/com/ds/user_service/configurations/JwtConfig.java
@@ -1,35 +1,16 @@
 package com.ds.user_service.configurations;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Setter
+@Getter
 @Configuration
-@ConfigurationProperties(prefix = "spring.jwt")
+@ConfigurationProperties(prefix = "jwt")
 public class JwtConfig {
     private String secret;
     private long expiration;
     private long jwtAccessExpiration;
-    public long getJwtAccessExpiration() {
-        return jwtAccessExpiration;
-    }
-
-    public void setJwtAccessExpiration(long jwtAccessExpiration) {
-        this.jwtAccessExpiration = jwtAccessExpiration;
-    }
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
-    public long getExpiration() {
-        return expiration;
-    }
-
-    public void setExpiration(long expiration) {
-        this.expiration = expiration;
-    }
 }
